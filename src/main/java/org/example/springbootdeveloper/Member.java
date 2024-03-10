4cdaadb581154103bcf7ef2a4fcbe034c594fbeb
KTH1007 <rlaxogjs2684@gmail.com>
2024-03-10 18:19:48 +0900

    feat: Member 관리를 위한 JPA

--- src/main/java/org/example/springbootdeveloper/Member.java
+++ src/main/java/org/example/springbootdeveloper/Member.java
@@ -6,17 +6,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access =  AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 @Entity
 
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id; // DB 테이블의 'id' 컬럼과 매칭
 
     @Column(name = "name", nullable = false)
-    private  String name; // DB 테이블의 'name; 컬럼과 매칭
+    private String name; // DB 테이블의 'name; 컬럼과 매칭
 }
